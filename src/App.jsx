fbfc0bac8dc9ffaa7e5544fff900d9f7a6158a14
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2023-06-07 22:11:32 -0400

    created navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navigation from './components/Navigation.jsx';
 import './App.css'
+import Bio from './components/Bio.jsx';
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navigation/>
+      {/* <Bio/> */}
     </div>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,112 +1,54 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import {GrLinkedin} from 'react-icons/gr'; 
+import laptopMemoji from "/src/assets/navigation/laptop-memoji.svg";
+import unqCircle from "/src/assets/navigation/unq-circle.svg";
+import linkedIn from "/src/assets/navigation/linkedIn.svg";
 import "./Navigation.css";
 
 function Navigation() {
-  const [activeLink, setActiveLink] = useState("home");
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
-  const onUpdateActiveLink = (value) => {
-    setActiveLink(value);
-  }
-
   return (
-    <>
-      <nav
-        className={
-          scrolled
-            ? "navbar navbar-expand-lg navigation scrolled"
-            : "navbar navbar-expand-lg navigation"
-        }
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img
-              src="/src/assets/laptop-memoji.svg"
-              alt="Logo Image"
-              width="50"
-              height="50"
-              className=""
-            />
-            Angelo Bennett
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark navigation">
+      <div className="container-fluid">
+        <div className="d-flex align-items-center">
+          <img src={laptopMemoji} alt="" className="mb-3 img-fluid" />
+          <a className="navbar-brand" href="#bio">
+            ANGELO BENNETT
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a
-                  className={
-                    activeLink === "home" ? "nav-link active" : "nav-link"
-                  }
-                  aria-current="page"
-                  href="#home"
-                  onClick={() => onUpdateActiveLink("home")}
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className={
-                    activeLink === "skills" ? "nav-link active" : "nav-link"
-                  }
-                  href="#skills"
-                  onClick={() => onUpdateActiveLink("skills")}
-                >
-                  Skills
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className={
-                    activeLink === "projects" ? "nav-link active" : "nav-link"
-                  }
-                  href="#projects"
-                  onClick={() => onUpdateActiveLink("projects")}
-                >
-                  Projects
-                </a>
-              </li>
-            </ul>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#">
-                  <img src={GrLinkedin} alt="" />
-                </a>
-              </div>
-              <button className="vvd" onClick="">
-                <span>Let's Connect</span>
-              </button>
-            </span>
-          </div>
         </div>
-      </nav>
-    </>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+            <li className="nav-item m-5">
+              <a className="nav-link active" aria-current="page" href="#">
+                Bio
+              </a>
+            </li>
+            <li className="nav-item m-5">
+              <a className="nav-link" href="#">
+                Skills
+              </a>
+            </li>
+            <li className="nav-item m-5">
+              <a className="nav-link" href="#">
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div>   
+          <img src={unqCircle} alt="" className="img-fluid m-2" />
+          <img src={linkedIn} alt="" className="img-fluid m-2" />
+          <button className="btn btn-light m-2 social-btn">Let's Connect</button>
+      </div>
+    </nav>
   );
 }
 
